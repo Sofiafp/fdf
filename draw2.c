e6822917fe45ed594eba0612f5e44222dae97033
Sofia <sofia@Air-de-Sofia.home>
2022-04-05 22:16:31 +0100

    Can conect points, only parallel lines

--- draw2.c
+++ draw2.c
@@ -6,7 +6,7 @@
 /*   By: salegre- <salegre-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 17:02:35 by salegre-          #+#    #+#             */
-/*   Updated: 2022/04/03 18:09:32 by salegre-         ###   ########.fr       */
+/*   Updated: 2022/04/05 22:15:32 by salegre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,34 @@ void	find_points(t_win *ptr, int **matrix, int x, int y)
 	//ft_putnbr_fd(matrix[i][j], 1);
 	int x_a = (x - matrix[i][j]) / sqrt(2);
 	int y_a = (x + 2*y + matrix[i][j]) / sqrt(6);
+	// int x_a_ = x_a;
+	// int y_a_ = y_a;
 
 	if (j + 1 != ptr->weight)
 	{
-		int x_b = (x - matrix[i][j + 1]) / sqrt(2);
-		int y_b = (x - matrix[i][j + 1]) / sqrt(2);
-	}
+		int x_b = (x + 25 - matrix[i][j + 1]) / sqrt(2);
+		int y_b = (x +25 + 2*y + matrix[i][j + 1]) / sqrt(6);
 
-	mlx_pixel_put(ptr, ptr->win, y_a, x_a, 0x11FFFFFF);
+		//float add = (y_b - y_a)/(x_b - x_a);
+		//float res = 0;
+		while (x_a != x_b)
+		{
+			// ft_putstr_fd("OIOI", 1);
+			mlx_pixel_put(ptr, ptr->win, y_a, x_a, 0x11FFFFFF);
+			y_a++;
+		}
+	}
+	// if (i + j != ptr->height)
+	// {
+	// 	int x_c = (x - matrix[i][j + 1]) / sqrt(2);
+	// 	while (x_a_ != x_c)
+	// 	{
+	// 		// ft_putstr_fd("OIOI", 1);
+	// 		mlx_pixel_put(ptr, ptr->win, y_a_, x_a_, 0x11FFFFFF);
+	// 		x_a_++;
+	// 	}				
+	// } 
+	//mlx_pixel_put(ptr, ptr->win, y_a, x_a, 0x11FFFFFF);
 	if (j == ptr->weight - 1)
 	{
 		j = 0;
